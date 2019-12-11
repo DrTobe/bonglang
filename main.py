e7433831d435b04a744ceafae46776a8f706f94f
sschellhoff <=soeren.schellhoff@gmail.com>
2019-12-11 12:46:13 +0100

    added main.py, run without arguments to start e repl or with one file as argument to evaluate.\nadded example folder with one simple example

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import lexer
+import parser
+import evaluator
+import repl
+
+def main():
+    arguments = sys.argv
+    if len(arguments) == 1:
+        return repl.main()
+    if len(arguments) == 2:
+        with open(arguments[1]) as f:
+            code = f.read()
+            l = lexer.Lexer(code)
+            p = parser.Parser(l)
+            program = p.compile()
+            evaluator.Eval().evaluate(program)
+    else:
+        print("Too many arguments\nrun without arguments to start the REPL or run with one file as argument to evaluate")
+
+if __name__ == "__main__":
+    main()
