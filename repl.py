2b571d199c28d0a47bc21f7466660748a1987ff7
DrTobe <7713259+DrTobe@users.noreply.github.com>
2019-12-12 01:34:42 +0100

    Turn into a real shell with the well-known [username@hostname directory]> pattern.

--- repl.py
+++ repl.py
@@ -4,12 +4,26 @@ from evaluator import Eval
 import traceback
 from symbol_table import SymbolTable
 
+import subprocess # now, we're going shell
+
+# it's a pity, I have to define this function, but bong is not yet
+# available :(
+def run(cmd):
+    res = subprocess.run(cmd, stdout=subprocess.PIPE, encoding="utf-8")
+    return res.stdout[:-1] # omit \n at the end
+
 def main():
     evaluator = Eval()
     symtable = SymbolTable()
     while True:
         try:
-            code = input(">")
+            # towards a nicer repl-experience
+            username = run("whoami")
+            hostname = run("hostname")
+            directory = run("pwd").split("/")[-1]
+            char = ">" if username!="root" else "#>"
+            repl_line = "[{}@{} {}]{} ".format(username, hostname, directory, char)
+            code = input(repl_line)
             if code == "q":
                 break
             code += "\n"
